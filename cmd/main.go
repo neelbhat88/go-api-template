fe607a9ac8d3fe67158adf2025a79f2fe88784f3
Neel Bhat <neel@tovala.com>
2024-07-29 23:34:59 -0500

    Adding request and response logging

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
+	"github.com/neelbhat88/go-api-template/internal/apimiddleware"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"net/http"
@@ -44,7 +45,8 @@ func main() {
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	r.Use(apimiddleware.RequestResponseLogger)
+	r.Use(middleware.Heartbeat("/ping"))
 	r.Use(middleware.Recoverer)
 
 	// Set a timeout value on the request context (ctx), that will signal
@@ -55,11 +57,17 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		name := r.URL.Query().Get("name")
 
+		// TODO: Use zerolog hook to create an internal log package that always logs requestID with each log entry
+		log.Info().Str("name", name).Msg("saying hi to someone")
+
 		if name == "world" {
+			// TODO: Make a helper function to return a response with a status code and message
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("Name cannot be 'world'"))
 			return
 		}
+
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
 	})
 
---
+++ internal/apimiddleware/request_response_logger.go
@@ -0,0 +1,72 @@
+package apimiddleware
+
+import (
+	"bytes"
+	"github.com/go-chi/chi/v5/middleware"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (rw *responseWriter) WriteHeader(status int) {
+	rw.status = status
+	rw.ResponseWriter.WriteHeader(status)
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	size, err := rw.ResponseWriter.Write(b)
+	rw.size += size
+	return size, err
+}
+
+func RequestResponseLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		var requestID string
+		if reqID := r.Context().Value(middleware.RequestIDKey); reqID != nil {
+			requestID = reqID.(string)
+		}
+
+		var requestBody bytes.Buffer
+		if r.Body != nil {
+			body, _ := io.ReadAll(r.Body)
+			requestBody.Write(body)
+			r.Body = io.NopCloser(bytes.NewBuffer(body))
+		}
+
+		rw := &responseWriter{ResponseWriter: w}
+
+		log.Info().
+			Str("_request_id", requestID).
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Str("params", r.URL.Query().Encode()).
+			Str("request_body", requestBody.String()).
+			Msg("Request received")
+
+		next.ServeHTTP(rw, r)
+
+		elapsed := time.Since(start)
+
+		logEvent := log.Info()
+		if rw.status >= 400 {
+			logEvent = log.Error()
+		}
+
+		logEvent.Str("_request_id", requestID).
+			Str("method", r.Method).
+			Str("path", r.URL.Path).
+			Int("status", rw.status).
+			Int("response_size_b", rw.size).
+			Float64("response_time_µs", float64(elapsed.Microseconds())).
+			Msg("Response sent")
+	})
+}
